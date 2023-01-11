b14b6af4f645bf5e69a6084da50b2365c1e13c5d
Alejandra Toro <torogrisalesa@gmail.com>
2023-01-11 17:15:35 -0500

    I've added the option of continue with the program or finish

--- src/main/java/Converter/Money/App.java
+++ src/main/java/Converter/Money/App.java
@@ -21,15 +21,20 @@ public class App
         	}
         	currencyTo = Currency.Symbols(currencyTo);
         	String amount = JOptionPane.showInputDialog("Amount");
-        	if(amount == null) {
+        	boolean isNumeric = (amount != null && amount.matches("[0-9]+"));
+        	if(!isNumeric) {
+        		JOptionPane.showMessageDialog(null,"Invalid value.Only numbers");
         		continue;
         	}
         	double response = Request.response(currencyTo,currencyFrom,amount);
         	JOptionPane.showMessageDialog(null, response);
+        	int confirm = JOptionPane.showConfirmDialog(null, "Do you want to continue?","Converter",JOptionPane.YES_NO_OPTION);
+        	if(JOptionPane.NO_OPTION == confirm) {
+        		confirmFrom = null;
+        		break;
+        	}
     	}
-    	
-    	
-    	
+        	
     }
     
 }
